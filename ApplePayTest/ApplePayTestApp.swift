f3d5600bf91215e6d7d5e2e745f421bb14f3f856
Johan Forsell <forsell.johan@hotmail.com>
2022-08-14 20:26:26 +0200

    Initial Commit

---
+++ ApplePayTest/ApplePayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePayTestApp.swift
+//  ApplePayTest
+//
+//  Created by Johan Forsell on 2022-08-14.
+//
+
+import SwiftUI
+
+@main
+struct ApplePayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApplePayTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApplePayTest
+//
+//  Created by Johan Forsell on 2022-08-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
